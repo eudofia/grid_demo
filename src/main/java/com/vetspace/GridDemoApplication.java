f6b84aef3ec4a4e791795f5d4d2d9c9028b99e17
Etimbuk Udofia <eudofia@vetspace.com>
2019-06-10 11:24:52 +0100

    Initial commit

---
+++ src/main/java/com/vetspace/GridDemoApplication.java
@@ -0,0 +1,12 @@
+package com.vetspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class GridDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vetspace/GridDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetspace;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) @SpringBootTest public class GridDemoApplicationTests {
+
+	@Test public void contextLoads() {
+	}
+
+}
